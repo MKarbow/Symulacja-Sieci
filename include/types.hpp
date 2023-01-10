f8f2a7cfaf5a17a86d1df1049f557e7a352ad3aa
MKarbow <114915115+MKarbow@users.noreply.github.com>
2023-01-10 15:53:19 +0100

    Add files via upload

---
+++ include/types.hpp
@@ -0,0 +1,14 @@
+
+#ifndef SIECI_TYPES_HPP
+#define SIECI_TYPES_HPP
+
+#include <set>
+#include <functional>
+
+using ElementID = unsigned int;
+using Time = int;
+using TimeOffset = int;
+using ProbabilityGenerator = std::function<double()>;
+
+#endif
+
