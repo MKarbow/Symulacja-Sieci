bc643ae4d2a471c472ea24f7e8dab0f34f076143
AdamJedrz <114914986+AdamJedrz@users.noreply.github.com>
2023-01-23 19:29:06 +0100

    wer1

--- include/types.hpp
+++ include/types.hpp
@@ -5,8 +5,5 @@
 #include <functional>
 
 using ElementID = unsigned int;
-using Time = int;
-using TimeOffset = int;
-using ProbabilityGenerator = std::function<double()>;
 
 #endif //SIECI_TYPES_HPP
